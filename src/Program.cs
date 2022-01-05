ecfd117f6c730c2fc914f53ce69ca14c66a531d8
林志興 <shingolin@gmail.com>
2022-01-05 16:41:12 +0800

    gg

--- src/Program.cs
+++ src/Program.cs
@@ -3,6 +3,6 @@ using CI_DEMO;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 MyCalc myCalc = new MyCalc();
-app.MapGet("/", () => "Hi!!Hello World!2022,2+3=" + myCalc.Add(2, 3).ToString());
+app.MapGet("/", () => "GG!!Hello World!2022,2+3=" + myCalc.Add(2, 3).ToString());
 
 app.Run();
