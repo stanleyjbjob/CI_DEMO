07b841a4df5b0ce512c0ce3a94fb76bbb5fe0ada
林志興 <shingolin@gmail.com>
2022-06-15 16:20:07 +0800

    test sql injection code issue

--- src/Program.cs
+++ src/Program.cs
@@ -4,5 +4,6 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 MyCalc myCalc = new MyCalc();
 app.MapGet("/", () => "GG!!Hello World!2022,2+3=" + myCalc.Add(2, 3).ToString());
-
+SqlQuery SqlQuery = new SqlQuery("SELECT * FROM HCODE WHERE H_CODE = 'H0001'", "Data Source=192.168.1.12;Initial Catalog=TASAMENGHR;Persist Security Info=True;User ID=jb;Password^Hsx9bu5t@");
+SqlQuery.GetData();
 app.Run();
---
+++ src/SqlQuery.cs
@@ -0,0 +1,34 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace CI_DEMO
+{
+    public class SqlQuery
+    {
+        public string Query { get; set; }
+        public string ConnectionString { get; set; }
+
+        public SqlQuery(string query, string connectionString)
+        {
+            Query = query;
+            ConnectionString = connectionString;            
+        }
+        public DataTable GetData()
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(Query, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        DataTable dataTable = new DataTable();
+                        dataTable.Load(reader);
+                        return dataTable;
+                    }
+
+            }
+            return null;
+        }
+    }
+}
