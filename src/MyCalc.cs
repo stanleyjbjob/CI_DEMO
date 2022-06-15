9af2b4ed2e7b18b30c44c4d7431ff94f02f6fdb7
林志興 <shingolin@gmail.com>
2022-06-15 15:44:16 +0800

    new method

--- src/MyCalc.cs
+++ src/MyCalc.cs
@@ -7,5 +7,17 @@
         {
             return x + y;
         }
+        public int Sub(int x, int y)
+        {
+            return x - y;
+        }
+        public int Mul(int x, int y)
+        {
+            return x * y;
+        }
+        public int Div(int x, int y)
+        {
+            return x / y;
+        }
     }
 }
--- test/TestProject1/UnitTest1.cs
+++ test/TestProject1/UnitTest1.cs
@@ -13,5 +13,26 @@ namespace TestProject1
             var result = calc.Add(1, 1);
             Assert.AreEqual(2, result, "1+1=2");
         }
+        [TestMethod]
+        public void MyCalc_Sub_Test1_1()
+        {
+            MyCalc calc = new MyCalc();
+            var result = calc.Sub(3, 1);
+            Assert.AreEqual(2, result, "3-1=2");
+        }
+        [TestMethod]
+        public void MyCalc_Mul_Test1_1()
+        {
+            MyCalc calc = new MyCalc();
+            var result = calc.Mul(3, 2);
+            Assert.AreEqual(6, result, "3*2=6");
+        }
+        [TestMethod]
+        public void MyCalc_Div_Test1_1()
+        {
+            MyCalc calc = new MyCalc();
+            var result = calc.Div(6, 2);
+            Assert.AreEqual(3, result, "6/2=3");
+        }
     }
 }
