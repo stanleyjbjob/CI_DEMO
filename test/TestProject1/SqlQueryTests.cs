4aff7007ca060d6b9c17e5048b69731029a0473e
林志興 <shingolin@gmail.com>
2022-06-16 10:18:45 +0800

    fake test

---
+++ test/TestProject1/SqlQueryTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CI_DEMO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CI_DEMO.Tests
+{
+    [TestClass()]
+    public class SqlQueryTests
+    {
+        [TestMethod()]
+        public void GetDataTest()
+        {
+            SqlQuery sqlQuery = new SqlQuery("SELECT * FROM HCODE WHERE H_CODE = 'H0001'", "Data Source=192.168.1.12;Initial Catalog=TASAMENGHR;Persist Security Info=True;User ID=jb;Password^Hsx9bu5t@");
+            var result = sqlQuery.GetData();
+            Assert.AreEqual(true, true);
+        }
+    }
+}
